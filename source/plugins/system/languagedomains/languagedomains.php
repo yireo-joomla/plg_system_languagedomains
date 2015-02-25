1ff50e91e70bb09623a83714c3c77dd02e35f3a2
Jisse Reitsma <info@yireo.com>
2015-02-25 13:22:13 +0100

    Change call to static variable into internal call

--- source/plugins/system/languagedomains/languagedomains.php
+++ source/plugins/system/languagedomains/languagedomains.php
@@ -83,7 +83,7 @@ class plgSystemLanguageDomains extends plgSystemLanguageFilter
 				}
 
 				// Make sure the current language-tag is registered as current
-				if (!empty($newLanguageTag) and $newLanguageTag != self::$default_lang)
+				if (!empty($newLanguageTag) and $newLanguageTag != $this->default_lang)
 				{
 					$this->setLanguage($newLanguageTag);
 				}
